701ad687c80ddbaf301f3d52981194376406cfc7
Uros Perisic <uros.m.perisic@gmail.com>
2023-07-14 21:19:04 +0800

    Settle on vitest

--- src/models/tag.test.ts
+++ src/models/tag.test.ts
@@ -1,21 +1,23 @@
-import test from 'ava'
+import { expect, it, describe } from 'vitest'
 import { makeTagId } from './tag'
 
-const table = [
-  { name: 'empty', in: '', out: '' },
-  { name: 'space', in: 'two words', out: 'two-words' },
-  { name: 'multi-space', in: 'two  words', out: 'two-words' },
-  { name: 'tab', in: 'two\twords', out: 'two-words' },
-  { name: 'multi-tab', in: 'two\t\twords', out: 'two-words' },
-  { name: 'mixed-space-tab', in: 'two\t words', out: 'two-words' },
-  { name: 'space-dash', in: 'two -words', out: 'two-words' },
-  { name: 'multi-dash', in: 'two--words', out: 'two-words' },
-  { name: 'space-multi-dash', in: 'two --words', out: 'two-words' },
-  { name: 'multi-space-multi-dash', in: 'two  --words', out: 'two-words' },
-]
+describe.concurrent('makeTagId', () => {
+  const table = [
+    { name: 'empty', in: '', out: '' },
+    { name: 'space', in: 'two words', out: 'two-words' },
+    { name: 'multi-space', in: 'two  words', out: 'two-words' },
+    { name: 'tab', in: 'two\twords', out: 'two-words' },
+    { name: 'multi-tab', in: 'two\t\twords', out: 'two-words' },
+    { name: 'mixed-space-tab', in: 'two\t words', out: 'two-words' },
+    { name: 'space-dash', in: 'two -words', out: 'two-words' },
+    { name: 'multi-dash', in: 'two--words', out: 'two-words' },
+    { name: 'space-multi-dash', in: 'two --words', out: 'two-words' },
+    { name: 'multi-space-multi-dash', in: 'two  --words', out: 'two-words' },
+  ]
 
-for (const row of table) {
-  test(row.name, (t) => {
-    t.is(makeTagId(row.in), row.out)
-  })
-}
+  for (const row of table) {
+    it(row.name, () => {
+      expect(makeTagId(row.in)).to.be.equal(row.out)
+    })
+  }
+})
